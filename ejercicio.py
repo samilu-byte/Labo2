2de605fe54f93090f385fb52b8204ace06db5eaa
samilu-byte <natalysortegap06@gmail.com>
2024-06-26 16:42:52 -0600

    Tarea2

---
+++ ejercicio.py
@@ -0,0 +1,13 @@
+print("Mezcla de colores \n")
+print("Opcion 1: Verde + Azul = \n")
+print("Opcion 2: Rojo + Verde \n")
+ingreso=int(input("Ingrese una opcion (1) (2): \n"))
+
+if ingreso == 1:
+    print("El color es Cyan")
+else:
+    if ingreso == 2:
+        print("El color es cafe")
+    else:
+        print("Solo hay 2 opciones")
+print("Gracias por usar nuestros servicios")
